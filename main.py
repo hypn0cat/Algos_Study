a341776e4cc93b46490fc93dd54bc083cf062568
Ruslan Sonkin <r.sonkinais@gmail.com>
2021-02-11 09:56:28 +0300

    make some changes in main.py file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from compare import compare
 from fib_funcs import *
 
-FIB_NUM = 800
+FIB_NUM = 30
 
 if __name__ == '__main__':
-    compare([fib2, fib3], list(range(FIB_NUM)))
+    compare([fib3, fib2, fib1], list(range(FIB_NUM)))
